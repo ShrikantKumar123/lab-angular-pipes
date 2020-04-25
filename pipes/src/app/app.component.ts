9626c103cbdc614b067a95f178988a05dee838cc
unknown <shrikant2994anil@gmail.com>
2020-04-25 22:17:01 +0530

    FSJ0320A005

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app-angularpipes';
+
+
+  name: string = 'Angular 4 Project!';
+  todaydate = new Date();
+  jsonval = { name: 'ajay', age: '25', address: { a1: 'Mumbai', a2: 'Karnataka' } };
+
+}
